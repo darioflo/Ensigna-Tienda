cf0df03e6c23c909f317da231c7367c321b6434b
darioflo <darioflo19@icloud.com>
2025-03-24 18:11:20 -0600

    Actualizado

--- src/app/components/card-team/card-team.component.ts
+++ src/app/components/card-team/card-team.component.ts
@@ -19,7 +19,7 @@ export class CardTeamComponent implements OnInit {
   ngOnInit(): void {
     this.productServices.getTeamPersons().subscribe({
       next: (data) => {
-        this.persons = data.results.slice(0, 8);
+        this.persons = data.results.slice(0, 9);
         console.log(this.persons);
       },
       error: (error) => {
