70cfbfb22fd456f426fb47ffa771587d9452938d
Darío Fernández <darioflo19@icloud.com>
2025-05-05 09:43:37 -0600

    Terminado si

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,25 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'productDetails/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      // Devuelve una lista de parámetros para prerenderizar
+      const productIds = ['1', '2', '3', '4', '5']; // Lista de IDs de productos
+      return productIds.map((id) => ({ id }));
+    },
+  },
+  {
+    path: 'clientDetails/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      // Devuelve una lista de parámetros para prerenderizar
+      const productIds = ['1', '2', '3', '4', '5']; // Lista de IDs de productos
+      return productIds.map((id) => ({ id }));
+    },
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
